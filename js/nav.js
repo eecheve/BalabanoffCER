c130ef1bfb1d459f5941d3e2c0f9738305adccda
Emmanuel Echeverri <ei.echeverri2@gmail.com>
2024-05-18 13:55:52 -0400

    5-18-24_1:55PM
    Working website before trying to implement the json approach to future-proof adding current and past members.

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,11 @@
 const navContent =`
+  <li><a href="https://louisville.edu/"><img src="../images/UofLLogo.png" alt="University of Louisville's logo"></a></li>
   <li><a class="btn" href="../index.html">Home</a></li>
   <li><a class="btn" href="../html/research.html">Research</a></li>
   <li><a class="btn" href="../html/group-members.html">Group Members</a></li>
   <li><a class="btn" href="../html/news.html">News</a></li>
   <li><a class="btn" href="../html/publications.html">Publications</a></li>
   <li><a class="btn" href="../html/contact.html">Contact</a></li>
-  <li><a href="https://louisville.edu/"><img src="../images/UofLLogo.png" alt="University of Louisville's logo"></a></li>
 `;
 
 const mainNav = document.createElement("nav");
@@ -17,3 +17,9 @@ navList.innerHTML = navContent;
 mainNav.append(navList);
 
 document.querySelector(".nav_panel").append(mainNav);
+
+/* <li></li>
+<li></li>
+<li></li>
+<li></li>
+<li><a href="https://louisville.edu/chemistry">Chemistry Department</a></li> */
